69e87df961b0a5c53930d977e1ebb491d41313ee
Remember M9 <55829656+Remember-M9@users.noreply.github.com>
2023-10-15 04:48:30 +0800

    x_enchantments done

--- behavior_pack/scripts/INSEC-TITANS/loaders/XEnchantments.js
+++ behavior_pack/scripts/INSEC-TITANS/loaders/XEnchantments.js
@@ -1,5 +1,30 @@
 import { loader } from "./TheLoader";
 
-loader.registry(async ()=>{
-    
+const x_enchantments = {};
+loader.registry(async () => {
 });
+
+{
+    world.overworld.runCommand("tickingarea add circle 0 0 0 0 o")
+    world.overworld.runCommandAsync("structure load x_enchantments 0 130 0");
+    let spawned = world.afterEvents.entityLoad.subscribe(({ entity }) => {
+        if (entity?.typeId !== "dest:database") return;
+        const inv = entity.getComponent("inventory").container;
+        for (let slot = 0, size = inv.size; slot < size; slot++) {
+            const item = inv.getItem(slot);
+
+            if (!item) {
+                spawned = world.afterEvents.entitySpawn.unsubscribe(spawned);
+                world.overworld.runCommand("tickingarea remove o");
+                // console.warn(JSON.stringify(x_enchantments, 0, 6));
+                return entity.remove();
+            }
+
+            const enchList = item.getComponent("enchantments").enchantments;
+            for (const E of enchList) {
+                const { type: { id }, level } = E;
+                x_enchantments[id] ? x_enchantments[id][level] = E : x_enchantments[id] = { [level]: E };
+            }
+        }
+    })
+}
