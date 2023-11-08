7b7499ece1398df4b3ae969e7d8e3605eb4d39c3
Dingsel <dshluka@gmail.com>
2023-11-08 02:02:50 +0100

    small db fix

--- behavior_pack/scripts/tests/db_test.js
+++ behavior_pack/scripts/tests/db_test.js
@@ -1,24 +1,39 @@
 import { FastingDB } from "utils/index";
 
 const db = new FastingDB()
-
-db.set("test1", false)
-
-if (db.get("test1") !== false) console.error("Test 1 for db failed : Expected value to be the boolean false but got " + db.get("test1"))
-
 db.clear()
-if (db.size !== 0) console.error("Either Clearing The DB or Getting Its Size Went Wrong")
 
-db.set("test2", "aaaaa".repeat(32000))
-if (db.get("test2") !== "aaaaa".repeat(32000)) console.error("Failed To Concatonate Strings. Got Value Was Not Set Value")
+//db.set("test1", false)
+//
+//if (db.get("test1") !== false) console.error("Test 1 for db failed : Expected value to be the boolean false but got " + db.get("test1"))
+//
+//db.clear()
+//if (db.size !== 0) console.error("Either Clearing The DB or Getting Its Size Went Wrong")
+//
+//db.set("test2", "aaaaa".repeat(32000))
+//if (db.get("test2") !== "aaaaa".repeat(32000)) console.error("Failed To Concatonate Strings. Got Value Was Not Set Value")
 
 const coords = { x: 20, y: 10, z: 100 }
 
 db.set("test3", coords)
 
-if(JSON.stringify(db.get("test3")) !== JSON.stringify(coords)) console.error("Failed To Recognise Vector3 Interface")
+if (JSON.stringify(db.get("test3")) !== JSON.stringify(coords)) console.error("Failed To Recognise Vector3 Interface")
 
-console.warn("DB Test Done")
+//db.set("test4", { test: "t", k: 231 })
+//
+//if (typeof db.get("test4") == "string") console.error("object is string")
+//
+//db.clear()
+//
+//const str = "aaa".repeat(32000) + "bbb".repeat(32000)
+//const str2 = "ccc".repeat(32000)
+//
+//db.set("test5", str)
+//db.set("test5", str2)
+//
+//if (db.get("test5") != str2) console.warn("YIKES")
+//
+//console.warn("DB Test Done")
 
 //db.set("test3", true)
 //db.set("test4", 31152)
--- behavior_pack/scripts/utils/fastingDb.js
+++ behavior_pack/scripts/utils/fastingDb.js
@@ -1,11 +1,13 @@
 import { World, world, Entity } from "@minecraft/server";
-const maxPropertyLength = 32000;
+const maxPropertyLength = Math.pow(2, 15) - 1;
 
 // prevents multiple instances for one dynamic property source
 const DB_Instances = new WeakMap();
 
 
 export class FastingDB {
+    /**@private */
+    vectorProps = ["x", "y", "z"]
     /**@private */
     storageType;
     /**@private */
@@ -17,25 +19,25 @@ export class FastingDB {
         if (DB_Instances.has(storageType)) return DB_Instances.get(storageType);
         DB_Instances.set(storageType, this);
         this.storageType = storageType;
-        this.__id = (storageType instanceof World) ? "WORLD_db_" : storageType.id + "_db_"
+        this.__id = (storageType instanceof World) ? "WORLD_db_" : storageType.id + "_db_";
     }
     /**
      * Returns A Filtered List Of Property Ids That Are Valid DB Entires
      * @private
      * @param {string | undefined} key 
-     * @returns {Array<string>}
+     * @returns {Array<string | undefined>}
      */
     getPropertyIds(key = undefined) {
-        return this.filtered(this.getPropertyIdsFromCache(), key)
+        return this.filtered(this.getPropertyIdsFromCache(), key);
     }
     /**
      * Rebuilds The Property Id Cache System
      * @private 
      **/
     updateCache() {
-        const allProps = this.storageType.getDynamicPropertyIds()
-        this.cache.set("ids", allProps)
-        return allProps
+        const allProps = this.storageType.getDynamicPropertyIds();
+        this.cache.set("ids", allProps);
+        return allProps;
     }
     /**
      * Returns Property IDs From The DB From The Cache And Rebuilds The Cahce If No Cached Data Exists
@@ -43,8 +45,8 @@ export class FastingDB {
      * @returns {Array<string>}
      */
     getPropertyIdsFromCache() {
-        if (this.cache.has("ids")) return this.cache.get("ids")
-        return this.updateCache()
+        if (this.cache.has("ids")) return this.cache.get("ids");
+        return this.updateCache();
     }
     /**
      * Returns An Array Of Elements Where Each Element Has To Start With The Entities Id And An Optional Key
@@ -54,28 +56,30 @@ export class FastingDB {
      * @returns {Array<string>}
      */
     filtered(val, key = undefined) {
-        const { __id } = this
-        return key ? val.filter(x => x.startsWith(__id + key)) : val.filter(x => x.startsWith(__id))
+        const { __id } = this;
+        return key ? val.filter(x => x.startsWith(__id + key)) : val.filter(x => x.startsWith(__id));
     }
     /**
      * @param {string} key 
      */
     get(key) {
-        const storages = this.getPropertyIds(key)
-        if (storages.length === 1) {
-
-            const nativeType = storages[0]
+        const { vectorProps } = this
+        const storages = this.getPropertyIds(key);
+        if (storages.length <= 1) {
+            const nativeType = this.storageType.getDynamicProperty(storages[0]);
             if (
-                typeof nativeType == "boolean"
+                !nativeType
+                || typeof nativeType == "boolean"
                 || typeof nativeType == "number"
-                || (Object.keys(nativeType).join("") === "xyz")
-            ) return nativeType
-            return JSON.parse(this.storageType.getDynamicProperty(storages[0]))
+                || (typeof nativeType == "object" && vectorProps.every(prop => typeof nativeType[prop] === 'number'))
+            ) return nativeType;
+            // @ts-ignore
+            return JSON.parse(nativeType);
         }
         return JSON.parse(storages.reduce((prev, curr) => {
-            prev += this.storageType.getDynamicProperty(curr)
-            return prev
-        }, ""))
+            prev += this.storageType.getDynamicProperty(curr);
+            return prev;
+        }, ""));
     }
 
 
@@ -85,14 +89,14 @@ export class FastingDB {
      * @param {any} value 
      */
     setString(key, value) {
-        const { storageType, __id } = this
-        const data = JSON.stringify(value)
+        const { storageType, __id } = this;
+        const data = JSON.stringify(value);
 
         let propertyCount = 0;
         for (let i = 0; i < data.length; i += maxPropertyLength) {
-            const chunk = data.substring(i, i + maxPropertyLength)
-            storageType.setDynamicProperty(__id + key + propertyCount, chunk)
-            propertyCount++
+            const chunk = data.substring(i, i + maxPropertyLength);
+            storageType.setDynamicProperty(__id + key + propertyCount, chunk);
+            propertyCount++;
         }
     }
 
@@ -102,64 +106,69 @@ export class FastingDB {
      * @param {any} value 
      */
     set(key, value) {
-        const { storageType, __id } = this
+        const { storageType, __id, vectorProps } = this;
 
-        switch (typeof data) {
-            case "number": { storageType.setDynamicProperty(__id + key + "0", data); break }
-            case "boolean": { storageType.setDynamicProperty(__id + key + "0", data); break }
+        this.getPropertyIdsFromCache().filter(x => x.startsWith(__id + key)).forEach(pId => {
+            storageType.setDynamicProperty(pId);
+        });
+
+        switch (typeof value) {
+            case "number": { storageType.setDynamicProperty(__id + key + "0", value); break; }
+            case "boolean": { storageType.setDynamicProperty(__id + key + "0", value); break; }
             case "object": {
-                Object.keys(data).join("") === "xyz" ?
-                    storageType.setDynamicProperty(__id + key + "0", data)
-                    : this.setString(key, value)
+                vectorProps.every(prop => typeof value[prop] === 'number') ?
+                    storageType.setDynamicProperty(__id + key + "0", value)
+                    : this.setString(key, value);
+                break;
             }
             default: {
-                this.setString(key, value)
+                this.setString(key, value);
             }
         }
-        this.updateCache()
-        return this
+        this.updateCache();
+        return this;
     }
 
     clear() {
-        const { storageType } = this
-        this.getPropertyIds().forEach((property) => storageType.setDynamicProperty(property))
-        this.updateCache()
+        const { storageType } = this;
+        this.getPropertyIds().forEach((property) => storageType.setDynamicProperty(property));
+        this.updateCache();
     }
 
     /**
      * @param {string} key
      */
     delete(key) {
-        const { storageType } = this
-        const props = this.getPropertyIds(key)
-        if (props.length === 0) return false
+        const { storageType } = this;
+        const props = this.getPropertyIds(key);
+        if (props.length === 0) return false;
         props
-            .forEach((property) => storageType.setDynamicProperty(property))
-        this.updateCache()
-        return true
+            .forEach((property) => storageType.setDynamicProperty(property));
+        this.updateCache();
+        return true;
     }
     get size() {
-        return this.getPropertyIds().length
+        return this.getPropertyIds().length;
     }
     entries() {
-        return this.constuctEntries()
+        return this.constuctEntries();
     }
     /** @param {string} key*/
     has(key) {
-        return !!this.getPropertyIds(key)
+        return !!this.getPropertyIds(key);
     }
     /** @param {(value : any, key : string, map : FastingDB) => void} func  */
     forEach(func) {
-        const entries = this.constuctEntries()
-        for (const [key, value] of entries) func(value, key, this)
+        const entries = this.constuctEntries();
+        for (const [key, value] of entries) func(value, key, this);
     }
     *keys() {
-        const entries = this.constuctEntries(true)
-        for (const [key] of entries) yield key
+        const entries = this.constuctEntries(true);
+        for (const [key] of entries) yield key;
     }
     *values() {
-        const entries = this.constuctEntries()
-        for (const [_, value] of entries) yield value
+        const entries = this.constuctEntries();
+        for (const [_, value] of entries) yield value;
     }
 
     /** 
@@ -167,15 +176,19 @@ export class FastingDB {
      * @private 
      */
     *constuctEntries(skipGet = false) {
-        const { __id } = this
-        const props = this.getPropertyIds()
+        const { __id } = this;
+        const props = this.getPropertyIds();
         /** @type {string[]}*/
-        const keys = []
+        const keys = [];
         for (const prop of props) {
-            const key = prop.substring(__id.length)
-            if (keys.includes(key)) continue
-            keys.push(key)
-            yield [key, skipGet ? null : this.get(key)]
+            const key = prop.substring(__id.length);
+            if (keys.includes(key)) continue;
+            keys.push(key);
+            yield [key, skipGet ? null : this.get(key)];
         }
+    }
+
+    *[Symbol.iterator]() {
+        return this.constuctEntries();
     }
 }
